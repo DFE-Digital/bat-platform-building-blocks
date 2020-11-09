9bc96a7f3d922ef44b860f43c092c6877e36c0b0
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2020-11-09 19:41:04 +0000

    Rename variables in exporter module
    This is to be more consistent with the other modules

--- terraform/modules/paas_prometheus_exporter/input.tf
+++ terraform/modules/paas_prometheus_exporter/input.tf
@@ -1,8 +1,15 @@
-variable space_id {}
-variable username {}
-variable password {}
-variable name {}
+variable monitoring_instance_name {}
+variable monitoring_space_id {}
+
+variable paas_username {}
+variable paas_password {}
 
 locals {
-  api_endpoint = "https://api.london.cloud.service.gov.uk"
+  docker_image_tag = "ae92e64f45264d450626cb33802b3649b68562d4"
+  paas_api_url     = "https://api.london.cloud.service.gov.uk"
+  environment_variable_map = {
+    API_ENDPOINT = local.paas_api_url
+    USERNAME     = var.paas_username
+    PASSWORD     = var.paas_password
+  }
 }
