8a4e364706c90cc8e758f536ff3ff3799d36ef55
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2020-11-09 19:16:52 +0000

    Dummy config for alertmanager
    This helps to deploy the module the first time

--- terraform/modules/alertmanager/input.tf
+++ terraform/modules/alertmanager/input.tf
@@ -1,3 +1,9 @@
-variable space_id {}
-variable name {}
-variable config {}
+variable monitoring_instance_name {}
+
+variable monitoring_space_id {}
+
+variable config { default = "" }
+
+locals {
+  config = var.config == "" ? file("${path.module}/config/alertmanager.yml") : var.config
+}
