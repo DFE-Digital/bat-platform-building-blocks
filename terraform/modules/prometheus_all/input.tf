092ed772fcca0ee68502a3bed4050452a833045e
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2020-11-09 19:45:50 +0000

    Refactor prometheus_all
    - Select which modules to install
    - Retrieve org and space data via datasources
    - Rename variables

--- terraform/modules/prometheus_all/input.tf
+++ terraform/modules/prometheus_all/input.tf
@@ -1,13 +1,26 @@
-variable space_id {}
+variable monitoring_instance_name {}
+variable monitoring_org_name {}
+variable monitoring_space_name {}
+
 variable paas_exporter_username {}
 variable paas_exporter_password {}
-variable name {}
-variable alertmanager_config {}
+
+variable alertmanager_config { default = "" }
+variable alert_rules { default = "" }
+
+variable grafana_google_client_id { default = "" }
+variable grafana_google_client_secret { default = "" }
 variable grafana_admin_password {}
-variable grafana_json_dashboards {
-  type    = list
-  default = []
-}
-variable alert_rules {
-  default = ""
+variable grafana_json_dashboards { default = [] }
+variable grafana_extra_datasources { default = [] }
+
+variable enabled_modules {
+  type = list
+  default = [
+    "paas_prometheus_exporter",
+    "prometheus",
+    "grafana",
+    "alertmanager",
+    "influxdb"
+  ]
 }
