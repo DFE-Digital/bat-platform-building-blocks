822f0eeee41e7ed402682cc2bb9755bc2144e0fd
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2020-11-17 11:19:15 +0000

    Export influxdb service-key

--- terraform/modules/influxdb/output.tf
+++ terraform/modules/influxdb/output.tf
@@ -1,3 +1,7 @@
 output service_instance_id {
   value = cloudfoundry_service_instance.influxdb.id
 }
+
+output credentials {
+  value = cloudfoundry_service_key.influxdb-key.credentials
+}
--- terraform/modules/influxdb/resources.tf
+++ terraform/modules/influxdb/resources.tf
@@ -3,3 +3,8 @@ resource cloudfoundry_service_instance influxdb {
   space        = var.monitoring_space_id
   service_plan = data.cloudfoundry_service.influxdb.service_plans["tiny-1_x"]
 }
+
+resource cloudfoundry_service_key influxdb-key {
+  name             = "prometheus-key"
+  service_instance = cloudfoundry_service_instance.influxdb.id
+}
