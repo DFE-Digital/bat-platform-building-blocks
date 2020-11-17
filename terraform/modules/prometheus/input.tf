95e8cd3415ba4bf304aaaf8288ee25f95c3a1312
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2020-11-17 11:20:08 +0000

    Allowed scrapes to be added to prometheus config file, rather than sent in as whole file

--- terraform/modules/prometheus/input.tf
+++ terraform/modules/prometheus/input.tf
@@ -4,6 +4,8 @@ variable monitoring_instance_name {}
 
 variable paas_prometheus_exporter_endpoint {}
 
+variable redis_prometheus_exporter_endpoint { default = "" }
+
 variable alertmanager_endpoint { default = "" }
 
 variable memory { default = 1024 }
@@ -12,20 +14,21 @@ variable disk_quota { default = 1024 }
 
 variable influxdb_service_instance_id {}
 
-variable alert_rules {
-  default = ""
-}
+variable alert_rules { default = "" }
 
-variable config_file {
-  default = ""
-}
+variable extra_scrape_config { default = "" }
 
 locals {
   template_variable_map = {
-    paas_prometheus_exporter_endpoint = var.paas_prometheus_exporter_endpoint
-    alertmanager_endpoint             = var.alertmanager_endpoint
-    paas_prometheus_exporter_name     = "paas-prometheus-exporter-${var.monitoring_instance_name}"
-    include_alerting                  = var.alert_rules == "" ? false : true
+    paas_prometheus_exporter_endpoint  = var.paas_prometheus_exporter_endpoint
+    redis_prometheus_exporter_endpoint = var.redis_prometheus_exporter_endpoint
+    alertmanager_endpoint              = var.alertmanager_endpoint
+    paas_prometheus_exporter_name      = "paas-prometheus-exporter-${var.monitoring_instance_name}"
+    include_alerting                   = var.alert_rules == "" ? false : true
+    include_redis_exporter             = var.redis_prometheus_exporter_endpoint == "" ? false : true
+    include_scrapes                    = var.extra_scrape_config == "" ? false : true
+    scrapes                            = var.extra_scrape_config
   }
-  config_file = var.config_file != "" ? var.config_file : templatefile("${path.module}/templates/prometheus.yml.tmpl", local.template_variable_map)
+
+  config_file = templatefile("${path.module}/templates/prometheus.yml.tmpl", local.template_variable_map)
 }
